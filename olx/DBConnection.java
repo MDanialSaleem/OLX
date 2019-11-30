d10b5351156c56cfa5fcb50a9bf77930f220118f
Haddi Amjad <hadiyoville25@gmail.com>
2019-11-30 18:43:07 +0500

    db connectivity updated

--- olx/DBConnection.java
+++ olx/DBConnection.java
@@ -1,5 +1,13 @@
 package olx;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+import java.sql.ResultSet;
+
+import java.time.LocalDate;
 
 //this should be singleton.
 public class DBConnection {
