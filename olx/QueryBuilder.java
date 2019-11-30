9c2640b8297eaef4cbb272c62fe0540b6e27060e
Danial Saleem <danialsaleem2010@gmail.com>
2019-11-30 12:00:10 +0500

    Fixes to constructors of some derived types. There were errors in paramter lists. Fixes to query QueryBuilder class. Use of wildcards has fixed the problems for now.

--- olx/QueryBuilder.java
+++ olx/QueryBuilder.java
@@ -3,5 +3,35 @@ package olx;
 import java.util.*;
 
 public class QueryBuilder {
-	List<>
+	List<Filter<? extends Comparable<?>>> filters = new ArrayList<>();
+	List<Advertisement> results;
+	
+	public QueryBuilder(List<Advertisement> initialResults) {
+		results = initialResults; 
+		//It may look like copying the list here is necessary but it is not because the filter methods do not modify 
+		//this list.
+	}
+	
+	private void apply() {
+		
+	}
+	public void addIntFilter(Filter<? extends Comparable<?>> filter) {
+		filters.add(filter);
+		apply();
+	}
+	public void removeFilter(Filter<? extends Comparable<?>> filter)
+	{
+		filters.remove(filter);
+		apply();
+	}
+	
+	
+	
+	public void sortByPrice() {
+		//tobeimplemened.
+	}
+	
+	public void sortByTime() {
+		//tobeimplemented.
+	}
 }
